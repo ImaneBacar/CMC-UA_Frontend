2eacf294b29f9e668ac65df25decfc4a39896e2a
BacarImane <imanebacar@outlook.fr>
2026-02-09 01:58:15 +0300

    ajout de la variable d'env

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://cmc-ua-backend.onrender.com'
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 // Intercepteur pour ajouter le token ET le rôle actif
